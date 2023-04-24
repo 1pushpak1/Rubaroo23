89452eb0935c85f65701db98048f994cd7d7ec65
Pushpak <pushpakkumawat20028@gmail.com>
2023-04-24 22:46:11 +0530

    update

--- res/js/main.js
+++ res/js/main.js
@@ -436,3 +436,15 @@ document.addEventListener("click", function(e) {
 
 
 
+jQuery(document).ready(function($) {
+  // auto timer
+  setTimeout(function() {
+    $('#lab-slide-bottom-popup').modal('show');
+  }, 5000); // optional - automatically opens in xxxx milliseconds
+
+  $(document).ready(function() {
+    $('.lab-slide-up').find('a').attr('data-toggle', 'modal');
+    $('.lab-slide-up').find('a').attr('data-target', '#lab-slide-bottom-popup');
+  });
+
+});
