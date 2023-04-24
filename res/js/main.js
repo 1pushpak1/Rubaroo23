8a86ca3492663cc18606796e1c9e5b4f42d2f1be
Pushpak <pushpakkumawat20028@gmail.com>
2023-04-25 01:03:04 +0530

    update

--- res/js/main.js
+++ res/js/main.js
@@ -438,9 +438,7 @@ document.addEventListener("click", function(e) {
 
 jQuery(document).ready(function($) {
   // auto timer
-  setTimeout(function() {
-    $('#lab-slide-bottom-popup').modal('show');
-  }, 5000); // optional - automatically opens in xxxx milliseconds
+  // optional - automatically opens in xxxx milliseconds
 
   $(document).ready(function() {
     $('.lab-slide-up').find('a').attr('data-toggle', 'modal');
